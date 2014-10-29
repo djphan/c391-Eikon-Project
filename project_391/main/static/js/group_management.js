bf4258e65ad8471c8b495770196b4c9dd92b57d8
jcairo <joncairo@gmail.com>
2014-10-29 16:20:24 -0600

    Added navbar links and remove display in group management

---
+++ project_391/main/static/js/group_management.js
@@ -0,0 +1,17 @@
+var groupManager = (function(){
+
+
+    return {
+        // create a post request and return
+        // array of json objects
+        clearMembers: function() {
+        },
+        
+        deleteMember: function(member, groupName){
+        },
+
+        createNodeObjects: function(jsonData) {
+        }
+    };
+})();
+
