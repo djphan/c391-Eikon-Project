bc30af2efa59b67797e54fc5e313b6b9a1d6cbfa
jcairo <joncairo@gmail.com>
2014-10-30 22:13:35 -0600

    Added ability to create new groups

--- project_391/main/static/js/group_management.js
+++ project_391/main/static/js/group_management.js
@@ -7,20 +7,53 @@ var groupManager = (function(){
             // make request for all groups and members
             // object should look like [{"groupName": "Brogrammers", "memberNames": ["Jon", "Carl", "Dan"]},... ]
             // endpoint /main/get_user_groups post {"username":"name"}
+            _this = this;
             var req = new XMLHttpRequest();
             req.onreadystatechange=function(){
                 if (req.readyState==4 && req.status == 200){
-                    this.userGroups = req.responseText;
+                    _this.userGroups = req.responseText;
+                    return _this.userGroups;
                 } else if (req.readyState==4 && req.status != 200){
-                    swal("Could not get group info");
+                    // swal("Could not get group info");
                 }
             };
             req.open("POST","/main/get_user_groups", true);
             req.setRequestHeader("Content-type", "application/json");
             req.send();
+            // return this.userGroups;
+            // object should look like [{"groupName": "Brogrammers", "memberNames": ["Jon", "Carl", "Dan"]},... ]
+            groupsInfo = [{"groupName": "Warriors", "memberNames": ["Jon", "Jim", "Jacob", "Jason", "Jimmy", "Jill"]},
+                  {"groupName": "Pets", "memberNames": ["Spot", "Speck", "Spike", "Spearmint", "Speedy", "Splash"]}];
+            this.userGroups = groupsInfo;
             return this.userGroups;
+
+        },
+        
+        // call this function when the user creates a new group
+        addNewGroupNameToList: function(groupName) {
+            // first make sure we can add the group on the server
+            // assuming add_group endpoint
+            _this = this;
+            var req = new XMLHttpRequest();
+            req.onreadystatechange=function(){
+                if (req.readyState==4 && req.status == 200){
+                    var newGroup = {"groupName": groupName, "groupMembers":[]};
+                    _this.userGroups.push(newGroup);
+                    _this.addGroupNamesToList(newGroup); 
+                } else if (req.readyState==4 && req.status != 200){
+                    var newGroup = {"groupName": groupName, "groupMembers":[]};
+                    swal("Could not add group");
+                }
+            };
+
+            // req.open("POST","/main/add_group", true);
+            req.open("POST","http://requestb.in/1638wbs1");
+            req.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+            // TODO body is not being set.
+            req.send(groupName);
         },
 
+        // call this function when adding a group that already exists to the list
         addGroupNamesToList: function(userGroups) {
             for (var i = 0; i < userGroups.length; i++){
                 var groupNameElement = document.createElement("a");
@@ -37,6 +70,11 @@ var groupManager = (function(){
         addGroupNameClickHandler: function(i, userGroups, groupNameElement) {
             _this = this;
             groupNameElement.addEventListener("click", function() {
+                var activeElements = groupNameElement.parentNode.getElementsByClassName("active");
+                for (var j = 0; j < activeElements.length; j++) {
+                    classie.remove(activeElements[j], 'active');
+                }
+                classie.add(groupNameElement, 'active');
                 _this.clearGroupMembersFromList();
                 _this.addGroupMembersToList(userGroups[i].memberNames, userGroups[i].groupName);
             }, 0);
@@ -105,16 +143,24 @@ var groupManager = (function(){
 window.onload = function() {
     // get an array of all groups and their members
     var groupsInfo = groupManager.getGroupInformation();
-    // object should look like [{"groupName": "Brogrammers", "memberNames": ["Jon", "Carl", "Dan"]},... ]
-    groupsInfo = [{"groupName": "Warriors", "memberNames": ["Jon", "Jim", "Jacob", "Jason", "Jimmy", "Jill"]},
-                  {"groupName": "Pets", "memberNames": ["Spot", "Speck", "Spike", "Spearmint", "Speedy", "Splash"]}];
-
-    // Populate the groups section and
+        // Populate the groups section and
     // display the members of the first list if there is one
     if (groupsInfo) {
         groupManager.addGroupNamesToList(groupsInfo);
-        groupManager.addGroupMembersToList(groupsInfo[0].members, groupsInfo[0].name); 
+        groupManager.addGroupMembersToList(groupsInfo[0].memberNames, groupsInfo[0].groupName); 
+        // show the first group as active since we show the first grouplist 
+        // by default.
+        var displayGroupElement = document.getElementsByClassName("group-names")[0];
+        classie.add(displayGroupElement.children[0], 'active');
     }
-    //groupManager.addGroupMembersToList(groupsInfo);
-    groupManager.addGroupMembersToList(["Jim"], "Beep");
+    // set up the add group button
+    var addGroupButton = document.getElementsByClassName("submit-new-group")[0];
+    addGroupButton.addEventListener("click", function() {
+        var groupNameTextField = document.getElementsByClassName("enter-group-name-field")[0];
+        swal(groupNameTextField.value);
+        groupManager.addNewGroupNameToList(groupNameTextField.value);
+    });
+    var addGroupSelectionBox = document.getElementsByClassName("group-select")[0];
+
 };
+
