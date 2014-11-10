5667c15f330d40492717f6eb0b3a5ee7988a1b2a
jcairo <joncairo@gmail.com>
2014-11-10 13:13:08 -0700

    Added postgres settings

--- project_391/project_391/settings.py
+++ project_391/project_391/settings.py
@@ -60,10 +60,21 @@ WSGI_APPLICATION = 'project_391.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'web_prj_db',
+        'HOST': '',
+        'PORT': '',
+        'PASSWORD': '',
+        'USER': '',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
