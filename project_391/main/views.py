1d9897b66efa4b3d4399854641273953df67573e
jcairo <joncairo@gmail.com>
2014-11-23 14:34:46 -0700

    Fixed issue with views not being counted properly.

--- project_391/main/views.py
+++ project_391/main/views.py
@@ -330,16 +330,16 @@ def get_image_data(request):
     # if the user performed a search, append the top 5 images first
     top_image_ids = []
     if "searchType" in params:
-        top_images = Views.objects.annotate(num_views=Count('photo_id')).order_by('num_views')
+        top_images = Views.objects.values("photo_id").annotate(Count("id")).order_by("-id__count")
         for idx, image in enumerate(top_images):
-            photo_id = image.photo_id.photo_id 
+            photo_id = image["photo_id"]
             top_image = Images.objects.get(photo_id=photo_id)
             top_image = serialize_image(top_image, user)
             top_image["topImage"] = "true"
             top_image["rank"] = str(idx + 1)
-            top_image["views"] = image.num_views
+            top_image["views"] = image["id__count"]
             # this handles the case where the 5th image is tied with the following images in view count.
-            if idx > 4 and response["images"][idx - 1]["views"] > image.num_views:
+            if idx > 4 and response["images"][idx - 1]["views"] > image["id__count"]:
                 break
             response["images"].append(top_image)
             top_image_ids.append(photo_id)
