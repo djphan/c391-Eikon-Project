351ad5126c7bf6f2d5b3c5033e711dcead7f156d
Carl Sobolewski <sobolews@ualberta.ca>
2014-11-25 01:17:43 -0700

    added datacube grouping by year/month/week

--- project_391/main/datacube.py
+++ project_391/main/datacube.py
@@ -1,18 +1,36 @@
 from django.db import models, connection
 
-def generateDataCube(owner_name=None, subject=None, timing=None):
-    dbquery = "SELECT {0}, {1}, {2}, count(*) FROM images GROUP BY {3};"
+def generateDataCube(owner_name=None, subject=None, timing=None, start_date=None, end_date=None):
+    assert timing in [None, 'year', 'week', 'month']
+    
+    dbquery = "SELECT {0}, {1}, {2}, count(*) FROM images {4} GROUP BY {3} {5};"
 
     zero = "owner_name" if owner_name else "null"
     one = "subject" if subject else "null"
-    two = "timing" if timing else "null"
+    two = "date_trunc('%s', timing)"%(timing) if timing else "null"
+
+    # GROUP BY clause
     three = ""
     if owner_name: three += "owner_name, "
     if subject:    three += "subject, "
-    if timing:     three += "timing, "
+    if timing:     three += "3, "
     if len(three) > 0: three = three[:-2]
 
-    dbquery = dbquery.format(zero, one, two, three)
+    # WHERE clause
+    four = ""
+    if start_date or end_date:
+        four += "WHERE "
+    if start_date:
+        four += "timing >= '%s' " % start_date
+    elif end_date:
+        four += "timing < ('%s'::date + '1 day'::interval) " % end_date
+    if end_date and start_date:
+        four += "AND timing < ('%s'::date + '1 day'::interval) " % end_date
+
+    # ORDER BY clause
+    five = "ORDER BY date_trunc('%s', timing)"%(timing) if timing else ''
+
+    dbquery = dbquery.format(zero, one, two, three, four, five)
     if not any([owner_name, subject, timing]):
         dbquery = dbquery[:-11]
     
@@ -29,27 +47,9 @@ def generateDataCube(owner_name=None, subject=None, timing=None):
         if subject:
             row["Subject"] = db_row[1]
         if timing:
-            row["Timing"] = db_row[2]
+            row["Timing"] = str(db_row[2].year) + '-' + str(db_row[2].month) + '-' + str(db_row[2].day)
         
         row["Count"] = db_row[3]
         json.append(row)
 
     return json
-
-# """
-# SELECT owner_name, subject, timing, count(*) as image_count FROM Images GROUP BY owner_name, subject, timing
-# UNION ALL
-# SELECT owner_name, null, timing, count(*) as image_count FROM Images GROUP BY owner_name, timing
-# UNION ALL
-# SELECT null, subject, timing, count(*) as image_count FROM Images GROUP BY subject, timing
-# UNION ALL
-# SELECT owner_name, subject, null, count(*) as image_count FROM Images GROUP BY owner_name, subject
-# UNION ALL
-# SELECT owner_name, null, null, count(*) as image_count FROM Images GROUP BY owner_name
-# UNION ALL
-# SELECT null, subject, null, count(*) as image_count FROM images GROUP BY subject
-# UNION ALL
-# SELECT null, null, timing, count(*) as image_count FROM images GROUP BY timing
-# UNION ALL
-# SELECT null, null, null, count(*) as image_count FROM images;
-# """
